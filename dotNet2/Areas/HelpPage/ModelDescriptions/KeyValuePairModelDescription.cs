0f81bcfc25de596a13ee5bf45d77e964e52d1662
WS-034-PRO\rcacuango <rcacuango@at4.net>
2017-07-11 16:43:45 +0200

    first commit

---
+++ dotNet2/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace dotNet2.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ dotNet2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dotNet2.WebApiApplication" Language="C#" %>
